9d643f537f427bbe5452c8ef4383baa35fb6321c
JennaLeysens <jennalicowley@gmail.com>
2020-05-22 23:24:20 +0200

    start details page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React from "react";
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import { Switch, Route } from "react-router-dom";
 import HomePage from "./Pages/HomePage";
 import AboutPage from "./Pages/AboutPage";
 import DiscoverMoviesPage from "./Pages/DiscoverMoviesPage";
 import NavBar from "./NavBar";
+import MoviePage from "./MoviePage";
 
 function App() {
   return (
     <div className="App">
       <NavBar> </NavBar>
       <Switch>
+        <Route path="/discover/:imdb_id" component={MoviePage} />
         <Route path="/discover" component={DiscoverMoviesPage} />
         <Route path="/about" component={AboutPage} />
         <Route path="/" component={HomePage} />
--- src/Pages/DiscoverMoviesPage.js
+++ src/Pages/DiscoverMoviesPage.js
@@ -1,5 +1,51 @@
-import React from "react";
+import React, { useState, useEffect, Link } from "react";
 
 export default function DiscoverMoviesPage() {
-  return <div>This is the DiscoverMoviesPage wheeeee.</div>;
+  const [searchText, set_searchText] = useState("");
+  const [searchState, setSearchState] = useState({ status: "idle" });
+  console.log(searchState);
+  const search = async () => {
+    console.log("Start searching for:", searchText);
+
+    const queryParam = encodeURIComponent(searchText);
+
+    setSearchState({ status: "searching" });
+
+    const data = await fetch(
+      `https://omdbapi.com/?apikey=fc6b182f&s=${queryParam}`
+    ).then((r) => r.json());
+
+    setSearchState({ status: "done", data: data });
+
+    console.log("Success!", data);
+  };
+
+  return (
+    <div className="container">
+      <h1>Discover some movies!</h1>
+
+      <input
+        value={searchText}
+        onChange={(e) => set_searchText(e.target.value)}
+      />
+      <button onClick={search}>Search</button>
+      <div className="row">
+        {searchState.status === "done" ? (
+          searchState.data.Search.map((movie) => {
+            return (
+              <div className="col-md-3">
+                <h6>
+                  {movie.Title} {movie.Year}
+                </h6>{" "}
+                <Link to="/discover/:imdb_id">{movie.imdbID} </Link>
+                <img src={movie.Poster} width="300px" height="400px" />
+              </div>
+            );
+          })
+        ) : searchState.status === "searching" ? (
+          <div className="container">Searching...</div>
+        ) : null}
+      </div>
+    </div>
+  );
 }
