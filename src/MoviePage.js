c44f9e25bd5a1593d09d469c8f6cf3c64b7b1950
JennaLeysens <jennalicowley@gmail.com>
2020-05-24 14:56:33 +0200

    complete

--- src/MoviePage.js
+++ src/MoviePage.js
@@ -1,7 +1,37 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 export default function MoviePage() {
-  const route_parameters = useParams();
-  // etc.
+  const [movieData, set_movieData] = useState({ state: "fetchingMovieData" });
+  const params = useParams();
+  const url = `/movie/${params.imdb_id}`;
+
+  useEffect(() => {
+    async function fetchData() {
+      const moviePage = await fetch(
+        `http://www.omdbapi.com/?apikey=fc6b182f&s&i=${params.imdb_id}`
+      ).then((r) => r.json());
+      console.log("hello", moviePage);
+      set_movieData({ state: "fetched", data: moviePage });
+    }
+
+    fetchData();
+  }, [params.imdb_id]);
+  console.log("help", movieData);
+  console.log("who?", params);
+  return (
+    <div className="container">
+      {movieData.state === "fetched" ? (
+        <div>
+          {" "}
+          <h2>{movieData.data.Title}</h2>{" "}
+          <h6>IMDB RATING: {movieData.data.imdbRating}</h6>
+          <h6>GENRE: {movieData.data.Genre}</h6>
+          <h6>PLOT: {movieData.data.Plot}</h6>
+          <img src={movieData.data.Poster} width="300px" height="400px" />
+        </div>
+      ) : null}
+    </div>
+  );
 }
--- src/Pages/DiscoverMoviesPage.js
+++ src/Pages/DiscoverMoviesPage.js
@@ -1,43 +1,55 @@
-import React, { useState, useEffect, Link } from "react";
+import React, { useState, useEffect } from "react";
+import { Link, useParams, useHistory } from "react-router-dom";
 
 export default function DiscoverMoviesPage() {
-  const [searchText, set_searchText] = useState("");
+  //   const [searchText, set_searchText] = useState("");
   const [searchState, setSearchState] = useState({ status: "idle" });
-  console.log(searchState);
-  const search = async () => {
-    console.log("Start searching for:", searchText);
+  console.log("search", searchState);
+  const history = useHistory();
+  const params = useParams();
+  console.log("what is this", params);
 
-    const queryParam = encodeURIComponent(searchText);
-
-    setSearchState({ status: "searching" });
-
-    const data = await fetch(
-      `https://omdbapi.com/?apikey=fc6b182f&s=${queryParam}`
-    ).then((r) => r.json());
-
-    setSearchState({ status: "done", data: data });
-
-    console.log("Success!", data);
+  const navigateToSearch = (searchText) => {
+    const routeParam = encodeURIComponent(searchText);
+    history.push(`/discover/${routeParam}`);
   };
+  console.log("sigh", searchState);
+
+  useEffect(() => {
+    async function searchMovie() {
+      if (params.searchtext === undefined) {
+        return;
+      } else {
+        const search = await fetch(
+          `https://omdbapi.com/?apikey=fc6b182f&s=${params.searchtext}`
+        ).then((r) => r.json());
+        setSearchState({ status: "done", data: search });
+      }
+    }
+    searchMovie();
+  }, [params.searchtext]);
 
   return (
     <div className="container">
       <h1>Discover some movies!</h1>
-
       <input
-        value={searchText}
-        onChange={(e) => set_searchText(e.target.value)}
+        value={params.searchtext}
+        onChange={(e) => navigateToSearch(e.target.value)}
       />
-      <button onClick={search}>Search</button>
+      {/* <button onClick={setSearchState}>Search</button> */}
       <div className="row">
-        {searchState.status === "done" ? (
+        {searchState.status === "done" && searchState.data.Search ? (
           searchState.data.Search.map((movie) => {
             return (
               <div className="col-md-3">
                 <h6>
-                  {movie.Title} {movie.Year}
+                  {
+                    <Link to={`/movie/${movie.imdbID}`}>
+                      {movie.Title} {movie.Year}
+                      <div>{movie.imdbID} </div>
+                    </Link>
+                  }
                 </h6>{" "}
-                <Link to="/discover/:imdb_id">{movie.imdbID} </Link>
                 <img src={movie.Poster} width="300px" height="400px" />
               </div>
             );
